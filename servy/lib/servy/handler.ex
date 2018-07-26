ae92dbcf703843f72d61b8bda2ef8a2a1ebc5d5c
Andris Strals <andris.strals@gmail.com>
2018-07-26 21:07:31 +0100

    Pledge controller added

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -26,13 +26,21 @@ defmodule Servy.Handler do
   end
 
 
+  def route(%Conv{method: "POST", path: "/pledges"} = conv) do
+    Servy.PledgeController.create(conv, conv.params)
+  end
+
+  def route(%Conv{method: "GET", path: "/pledges"} = conv) do
+    Servy.PledgeController.index(conv)
+  end
+
   def route(%Conv{method: "GET", path: "/sensors"} = conv) do
 
     task = Task.async(fn -> Tracker.get_location("bigfoot") end)
     shots =
-    ["cam-1", "cam-2", "cam-3"]
-           |> Enum.map(&Task.async(fn -> VideoCam.get_snapshot(&1) end))
-           |> Enum.map(&Task.await/1)
+      ["cam-1", "cam-2", "cam-3"]
+      |> Enum.map(&Task.async(fn -> VideoCam.get_snapshot(&1) end))
+      |> Enum.map(&Task.await/1)
 
     where_is_bigfoot = Task.await(task)
 
