403500e464666cec895c827f0df264ec74cae467
Andris Strals <andris.strals@gmail.com>
2018-07-27 21:00:49 +0100

    Pledge server provide sync functions to store and read pledges

--- servy/lib/servy/pledge_server.ex
+++ servy/lib/servy/pledge_server.ex
@@ -1,16 +1,54 @@
 defmodule Servy.PledgeServer do
 
-  def create_pledge(name, amount) do
-    {:ok, _id} = send_pledge_to_service(name, amount)
 
-    [ {"larry", 10}]
+  def start do
+    IO.puts "Starting the pledge server..."
+    spawn(__MODULE__, :listen_loop, [[]])
   end
 
-  def recent_pledges do
-    [ {"larry", 10}]
+  def listen_loop(state) do
+    IO.puts "\nWaiting for a message..."
+
+    receive do
+      {sender, :create_pledge, name, amount} ->
+        {:ok, id} = send_pledge_to_service(name, amount)
+        new_state = [{name, amount} | Enum.take(state, 2)]
+        send sender, {:response, id}
+        listen_loop(new_state)
+
+      {sender, :recent_pledges} ->
+        send sender, {:response, state}
+        listen_loop(state)
+    end
   end
+
+
+  def create_pledge(pid, name, amount) do
+    send pid, {self(), :create_pledge, name, amount}
+    receive do {:response, status} -> status end
+  end
+
+  def recent_pledges(pid) do
+    send pid, {self(), :recent_pledges}
+    receive do {:response, pledges} -> pledges end
+  end
+
   defp send_pledge_to_service(_name, _amount) do
-#    code goes here to send to external service
+    #    code goes here to send to external service
     {:ok, "pledge-#{:rand.uniform(1000)}"}
   end
+
+
+#  alias Servy.PledgeServer
+#    pid = PledgeServer.start
+#  #
+  #  Servy.PledgeServer.create_pledge(pid, "lary", 10)
+  #  Servy.PledgeServer.create_pledge(pid, "moe", 20)
+  #  Servy.PledgeServer.create_pledge(pid, "curly", 30)
+  #  Servy.PledgeServer.create_pledge(pid, "daisy", 40)
+  #  Servy.PledgeServer.create_pledge(pid, "grace", 50)
+
+  #  send pid, { self(), :recent_pledges}
+  #  receive do {:response, pledges} -> IO.inspect(pledges) end
+
 end
