811743c3b05a391795be1461f127abb9ca81d328
Andris Strals <andris.strals@gmail.com>
2018-08-07 22:43:16 +0100

    Pledge cache size adjustable

--- servy/lib/servy/pledge_server.ex
+++ servy/lib/servy/pledge_server.ex
@@ -5,10 +5,14 @@ defmodule Servy.PledgeServer do
   #  alias Servy.GenericServer
   use GenServer
 
+  defmodule State do
+    defstruct cache_size: 3, pledges: []
+  end
+
   #  Client interface functions
   def start do
     IO.puts "Starting the pledge server..."
-    GenServer.start(__MODULE__, [], name: @pid)
+    GenServer.start(__MODULE__, %State{}, name: @pid)
   end
 
   def create_pledge(name, amount) do
@@ -27,27 +31,37 @@ defmodule Servy.PledgeServer do
     GenServer.cast @pid, :clear
   end
 
+  def set_cache_size(size) do
+    GenServer.cast @pid, {:set_cache_size, size}
+  end
+
 
   # Server callbacks
   def handle_call(:total_pledged, _from, state) do
-    total = Enum.map(state, &elem(&1, 1))
+    total = Enum.map(state.pledges, &elem(&1, 1))
             |> Enum.sum
     {:reply, total, state}
   end
 
   def handle_call(:recent_pledges, _from, state) do
-    {:reply, state, state}
+    {:reply, state.pledges, state}
   end
 
   def handle_call({:create_pledge, name, amount}, _from, state) do
     {:ok, id} = send_pledge_to_service(name, amount)
-    new_state = [{name, amount} | Enum.take(state, 2)]
+    recent = Enum.take(state.pledges, (state.cache_size - 1))
+    cached_pledges = [{name, amount} | recent]
+    new_state = %{state | pledges: cached_pledges}
     {:reply, id, new_state}
   end
 
 
-  def handle_cast(:clear, _state) do
-    {:noreply, []}
+  def handle_cast(:clear, state) do
+    {:noreply, %{state | pledges: []}}
+  end
+
+  def handle_cast({:set_cache_size, size}, state) do
+    {:noreply, %{state | cache_size: size}}
   end
 
 
@@ -57,5 +71,4 @@ defmodule Servy.PledgeServer do
   end
 
 end
-
 
--- servy/test/pledge_server_test.exs
+++ servy/test/pledge_server_test.exs
@@ -22,6 +22,19 @@ defmodule PledgyTest do
     assert PledgeServer.recent_pledges() === []
     assert PledgeServer.total_pledged() === 0
 
+    PledgeServer.set_cache_size(4)
+    PledgeServer.create_pledge("larry", 10)
+    PledgeServer.create_pledge("moe", 20)
+    PledgeServer.create_pledge("curly", 30)
+    PledgeServer.create_pledge("daisy", 40)
+    PledgeServer.create_pledge("grace", 50)
+
+    pledges = PledgeServer.recent_pledges()
+    total = PledgeServer.total_pledged()
+
+    assert pledges === [{"grace", 50}, {"daisy", 40}, {"curly", 30}, {"moe", 20}]
+    assert total === 140
+
   end
 
 end
