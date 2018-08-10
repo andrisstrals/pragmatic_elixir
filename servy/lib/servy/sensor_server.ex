26588df56f17bbf42eb3f2ae2f0bc4284fd0c329
Andris Strals <andris.strals@gmail.com>
2018-08-11 00:49:49 +0100

    SensorServer refresh interval set at runtime

--- servy/lib/servy/sensor_server.ex
+++ servy/lib/servy/sensor_server.ex
@@ -2,42 +2,51 @@ defmodule Servy.SensorServer do
   @moduledoc false
 
   @name :sensor_server
-  @refresh_interval :timer.seconds(5)   #  :timer.minutes(60)
 
+  defmodule State do
+    defstruct sensor_data: %{}, refresh_interval: :timer.seconds(5)
+  end
 
   use GenServer
 
   def start do
-    GenServer.start(__MODULE__, %{}, name: @name)
+    GenServer.start(__MODULE__, %State{}, name: @name)
   end
 
   def get_sensor_data do
     GenServer.call(@name, :get_sensor_data)
   end
 
-  def init(_opts) do
-    initial_state = run_tasks_to_get_sensor_data()
-    schedule_refresh()
-    {:ok, initial_state}
+  def set_refresh_interval(interval) do
+    GenServer.cast(@name, {:set_interval, interval})
+  end
+
+
+#  Server callbacks
+
+  def init(state) do
+    initial_data = run_tasks_to_get_sensor_data()
+    schedule_refresh(state.refresh_interval)
+    {:ok, %{state | sensor_data: initial_data}}
   end
 
   def handle_call(:get_sensor_data, _from, state) do
-    {:reply, state, state}
+    {:reply, state.sensor_data, state}
   end
 
-  def handle_cast(_msg, state) do
-    {:noreply, state}
+  def handle_cast( {:set_interval, interval}, state) do
+    {:noreply, %{ state | refresh_interval: interval}}
   end
 
-  def handle_info(:refresh, _state) do
+  def handle_info(:refresh, state) do
     IO.puts "Refreshing sensor data cache..."
-    new_state = run_tasks_to_get_sensor_data()
-    schedule_refresh()
-    {:noreply, new_state}
+    sensor_data = run_tasks_to_get_sensor_data()
+    schedule_refresh(state.refresh_interval)
+    {:noreply, %{state | sensor_data: sensor_data}}
   end
 
   defp run_tasks_to_get_sensor_data do
-    IO.puts "Running tasks to get sensor data..."
+    IO.puts "Running tasks to get sensor data... #{Time.utc_now()}"
     task = Task.async(fn -> Servy.Tracker.get_location("bigfoot") end)
 
     snapshots =
@@ -50,7 +59,7 @@ defmodule Servy.SensorServer do
     %{snapshots: snapshots, location: where_is_bigfoot}
   end
 
-  defp schedule_refresh do
-    Process.send_after(self(), :refresh, @refresh_interval)  #direct message; handled in handle_info callback
+  defp schedule_refresh(refresh_interval) do
+    Process.send_after(self(), :refresh, refresh_interval)  #direct message; handled in handle_info callback
   end
 end
