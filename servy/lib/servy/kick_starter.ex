8a1b01489c146eecc5260932101e64a75d407931
Andris Strals <andris.strals@gmail.com>
2018-08-12 00:53:29 +0100

    Can get HTTP server pid from KickStarter

--- servy/lib/servy/kick_starter.ex
+++ servy/lib/servy/kick_starter.ex
@@ -8,6 +8,10 @@ defmodule Servy.KickStarter do
     GenServer.start(__MODULE__, :ok, name: __MODULE__)
   end
 
+  def get_server do
+    GenServer.call(__MODULE__, :get_server)
+  end
+
   def init(:ok) do
     Process.flag(:trap_exit, true)
     {:ok, start_server()}
@@ -17,6 +21,11 @@ defmodule Servy.KickStarter do
     IO.puts "It seems like shit happened to HTTP server... reason:#{inspect reason} state:#{inspect state}"
     {:noreply, start_server()}
   end
+
+  def handle_call(:get_server, _from, state) do
+    {:reply, state, state}
+  end
+
 
   defp start_server do
     IO.puts "Stasrting HTTP server"
